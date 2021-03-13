c8c22e58bd8715e29b6c4f89e4ba3af60bd83b78
Tyler Kindy <tylerkindy@gmail.com>
2021-03-13 10:09:48 -0500

    Match some two-character lexemes

--- src/main/java/com/tylerkindy/lox/Scanner.java
+++ src/main/java/com/tylerkindy/lox/Scanner.java
@@ -64,6 +64,18 @@ class Scanner {
       case '*':
         addToken(STAR);
         break;
+      case '!':
+        addToken(match('=') ? BANG_EQUAL : BANG);
+        break;
+      case '=':
+        addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+        break;
+      case '<':
+        addToken(match('=') ? LESS_EQUAL : LESS);
+        break;
+      case '>':
+        addToken(match('=') ? GREATER_EQUAL : GREATER);
+        break;
       default:
         Lox.error(line, "Unexpected character.");
         break;
@@ -81,5 +93,13 @@ class Scanner {
   private void addToken(TokenType type, Object literal) {
     String text = source.substring(start, current);
     tokens.add(new Token(type, text, literal, line));
+  }
+
+  private boolean match(char expected) {
+    if (isAtEnd()) return false;
+    if (source.charAt(current) != expected) return false;
+
+    current++;
+    return true;
   }
 }
