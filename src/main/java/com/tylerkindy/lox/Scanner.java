a5944c237ba7c4a933df6aa5934ed1bc20c2d9ad
Tyler Kindy <tylerkindy@gmail.com>
2021-03-14 09:21:52 -0400

    Scan numbers

--- src/main/java/com/tylerkindy/lox/Scanner.java
+++ src/main/java/com/tylerkindy/lox/Scanner.java
@@ -96,7 +96,11 @@ class Scanner {
         string();
         break;
       default:
-        Lox.error(line, "Unexpected character.");
+        if (isDigit(c)) {
+          number();
+        } else {
+          Lox.error(line, "Unexpected character.");
+        }
         break;
     }
   }
@@ -144,5 +148,28 @@ class Scanner {
   private char peek() {
     if (isAtEnd()) return '\0';
     return source.charAt(current);
+  }
+
+  private char peekNext() {
+    if (current + 1 >= source.length()) return '\0';
+    return source.charAt(current + 1);
+  }
+
+  private boolean isDigit(char c) {
+    return c >= '0' && c <= '9';
+  }
+
+  private void number() {
+    while (isDigit(peek())) advance();
+
+    // Look for a fractional part.
+    if (peek() == '.' && isDigit(peekNext())) {
+      // Consume the "."
+      advance();
+
+      while (isDigit(peek())) advance();
+    }
+
+    addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
   }
 }
