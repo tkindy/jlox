33515ab24c277b9223c3711f9bf51aa3d1f3a693
Tyler Kindy <tylerkindy@gmail.com>
2021-03-20 11:25:38 -0400

    Implement visitor pattern
    Since this is a sealed hierarchy, I think I could use the new instanceof
    pattern matching to avoid the need for the visitor pattern. But it seems
    like that won't be as efficient as visitors because it'll take O(n)
    checks to find the right branch to execute as opposed to the O(1) for
    the visitor polymorphic method dispatch.
    
    It seems like extending switch with the new instanceof pattern matching
    is in the offing, and that could probably be optimized to be O(1).

--- src/main/java/com/tylerkindy/lox/Expr.java
+++ src/main/java/com/tylerkindy/lox/Expr.java
@@ -1,8 +1,37 @@
 package com.tylerkindy.lox;
 
 sealed interface Expr {
-  record Binary(Expr left, Token operator, Expr right) implements Expr {}
-  record Grouping(Expr expression) implements Expr {}
-  record Literal(Object value) implements Expr {}
-  record Unary(Token operator, Expr right) implements Expr {}
+  record Binary(Expr left, Token operator, Expr right) implements Expr {
+    @Override
+    public <R> R accept(Visitor<R> visitor) {
+      return visitor.visitBinaryExpr(this);
+    }
+  }
+  record Grouping(Expr expression) implements Expr {
+    @Override
+    public <R> R accept(Visitor<R> visitor) {
+      return visitor.visitGroupingExpr(this);
+    }
+  }
+  record Literal(Object value) implements Expr {
+    @Override
+    public <R> R accept(Visitor<R> visitor) {
+      return visitor.visitLiteralExpr(this);
+    }
+  }
+  record Unary(Token operator, Expr right) implements Expr {
+    @Override
+    public <R> R accept(Visitor<R> visitor) {
+      return visitor.visitUnaryExpr(this);
+    }
+  }
+
+  <R> R accept(Visitor<R> visitor);
+
+  interface Visitor<R> {
+    R visitBinaryExpr(Binary expr);
+    R visitGroupingExpr(Grouping expr);
+    R visitLiteralExpr(Literal expr);
+    R visitUnaryExpr(Unary expr);
+  }
 }
